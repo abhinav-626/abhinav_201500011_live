0007075d0c7f6000e7a542543dc97a6b2809a984
abhinav-626 <80679609+abhinav-626@users.noreply.github.com>
2022-07-11 23:33:27 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,12 @@
+var name1 = document.getElementById('name');
+var clgroll = document.getElementById('rollno');
+var clgname = document.getElementById('clgname');
+var date = document.getElementById('date');
+var today = new Date();
+window.onload =()=>{
+    name1.innerText = "Abhinav Bajpai";
+    clgroll.innerText = "201500011";
+    clgname.innerText = "GLA University";
+    date.innerText = "11-07-2022";
+
+}
